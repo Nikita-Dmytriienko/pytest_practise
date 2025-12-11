789ac5f33f4f2a38b93af68efd0f920bb083da0f
Nikita-Dmytriienko <nik.dmitrienko2000@gmail.com>
2025-12-11 12:18:51 +0200

    test(calculator): add parameterized tests for Calculator.add and Calculator.divide methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,25 +1,28 @@
 import pytest   
 from src.calculator import Calculator
 
+class TestCalculator():
 
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 0.5),
-        (5, -1, -5),
-    ]
-                         )
-def test_divide(x, y, res):
-    assert Calculator().divide(x,y) == res
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 0.5),
+            (5, -1, -5),
+            (10, 5, 2),
+        ]
+                            )
+    def test_divide(self, x, y, res):
+        assert Calculator().divide(x,y) == res
 
-
-@pytest.mark.parametrize(
-    "x, y, res",
-    [
-        (1, 2, 3),
-        (5, -1, 4),
-    ]
-                         )
-def test_add(x, y, res):
-    assert Calculator().add(x,y) == res
+    @pytest.mark.parametrize(
+        "x, y, res",
+        [
+            (1, 2, 3),
+            (5, -1, 4),
+            (1, "3", 4),
+        ]
+                            )
+    def test_add(self, x, y, res):
+        assert Calculator().add(x,y) == res
     
+
